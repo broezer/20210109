32e167623b660d8af04339535faeb21cd2cb637d
broezer <bruce@moerdjiman.com>
2021-01-09 12:18:24 +0100

    ice ice baby

--- sketch_210109a.js
+++ sketch_210109a.js
@@ -5,8 +5,8 @@ let c1, c2;
 
 function setup() {
   createCanvas(400, 400);
-  c1 = color(126, 255, 148);
-  c2 = color(255, 106, 171);
+  c1 = color(131, 246, 255);
+  c2 = color(234, 253, 255);
 }
 
 
@@ -14,23 +14,24 @@ function draw() {
   setGradient(0, 0, width, height, c1, c2, Y_AXIS);
   
   
-  translate((-width/4.8), 0);
+  translate( -width*0.11, height*0.1);
   for (let i = 1; i <=3; i++) {
     let lineX = (i * (width * 0.15)) + (i * (width * 0.15));
-    setGradient( lineX , height*0.1, width*0.2, height*0.2, c2, c1, X_AXIS);
+    grdCircle2(lineX, height*0.1, width*0.2, c1, c2);
+    
   }
   
   for (let i = 1; i <=3; i++) {
-    let lineX = (i * (width * 0.15)) + (i * (width * 0.15));
-    setGradient( lineX , height*0.1 + height*0.3, width*0.2, height*0.2, c1, c2, X_AXIS);
-  }
+    let lineX = (i * (width * 0.15)) + (i * (width * 0.15));  
+    grdCircle2(lineX, height*0.1 + height*0.3, width*0.2, c2, c1);
+}
   
   for (let i = 1; i <=3; i++) {
     let lineX = (i * (width * 0.15)) + (i * (width * 0.15));
-    setGradient( lineX , height*0.1 + height*0.6, width*0.2, height*0.2, c2, c1, X_AXIS);
-  }
+    grdCircle2(lineX, height*0.1 + height*0.6, width*0.2, c1, c2);
+}
   
-  save("20210108.png");
+  save("20210109.png");
   noLoop();
 }
 
@@ -54,4 +55,17 @@ function setGradient(x, y, w, h, c1, c2, axis) {
       line(i, y, i, y + h);
     }
   }
+}
+
+function grdCircle2(x, y, d, c1, c2) {
+ let c = 100;
+ for (let i=0; i<c; i++) {
+   let col = lerpColor(c1, c2, i/c);
+   let a = lerp(PI, 0, i/c);
+   
+   fill(col);
+   noStroke();
+   arc(x, y, d, d, -a, a, CHORD);
+
+ }
 }
